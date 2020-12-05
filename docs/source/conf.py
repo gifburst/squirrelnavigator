52d06b8ea17e71ff098af67147c7df6fdc55cdf5
RIDERIUS <riderius.help@gmail.com>
2020-12-05 19:01:03 +0300

    Update Docs requirments

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ release = '1.5.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme", "sphinxcontrib.napoleon"]
+extensions = ["sphinx_rtd_theme", "sphinxcontrib.napoleon", "Akasia"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
